33d508e97ee885c6f7125b7a33c73792e80ffd83
Prasad sawant <49979483+QikTech@users.noreply.github.com>
2024-12-17 11:03:46 +0000

    datatypes

---
+++ js_practice/datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+// alert (2+4) // will not work here, use browser to see alert
+
+console.log(4+4)
+
+let name = "prasad"
+let age = 28
+let isLoggedIn = false
+/*
+// number ->2 to power 53
+bigint
+string -> ""
+bolean -> true/false
+null -> standalone value/type, empty value
+undefined -> value not assigned.defined - special type
+symbol -> unique
+
+object
+
+
+*/
