255d59108f5c7e78f04da256188f5efe2421c00b
qiktech <prassaw@gmail.com>
2024-12-18 01:09:38 +0530

    numbers And Math

---
+++ js_practice/numAndMath.js
@@ -0,0 +1,34 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(4));
+
+const otherNumber = 44.88
+// console.log(otherNumber.toPrecision(2));
+
+const hundreds = 1000000000
+// console.log(hundreds.toLocaleString('en-IN'));
+// console.log(hundreds.toLocaleString());
+
+// MATH ++++++++++++++++++++++++++++++++++++++++++++++++++
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(44.66));
+// console.log(Math.ceil(44.22));
+// console.log(Math.floor(44.99));
+// console.log(Math.min(1,2,6,7,3,2));
+// console.log(Math.max(1,2,6,7,3,2));
+
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log((Math.random()*10) + 1);
+
+const min = 10 
+const max = 20 
+
+console.log(Math.floor(Math.random() * (max-min + 1)) + min);
