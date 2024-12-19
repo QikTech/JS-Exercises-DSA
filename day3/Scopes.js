f37ccf24f519effff24160d5cf26e41856de47ba
qiktech <prassaw@gmail.com>
2024-12-19 13:32:38 +0000

    scopes

---
+++ day3/Scopes.js
@@ -0,0 +1,19 @@
+// GLOBAL SCOPE
+// let a = 10
+// const b = 20
+// var c =30
+
+
+if (true) {
+    // BLOCK SCOPE
+    let a = 10
+    const b = 20
+    var c = 30
+    d = 40
+}
+
+// console.log(a);
+// console.log(b);
+console.log(c);
+console.log(d);
+
