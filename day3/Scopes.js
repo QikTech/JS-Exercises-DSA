780da1285f9080d3f59b8fba3f8bdc517582f755
qiktech <prassaw@gmail.com>
2024-12-19 13:47:53 +0000

    scope level & hoisting

--- day3/Scopes.js
+++ day3/Scopes.js
@@ -17,3 +17,42 @@ if (true) {
 console.log(c);
 console.log(d);
 
+https://www.youtube.com/watch?v=eWwge2YpHhc&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=22
+function one(){
+    const username = "Prasad"
+
+    function two(){
+        const website = "Youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    
+    two()
+}
+
+one()
+
+if(true) {
+    const username = "Prasad"
+    if(username === "Prasad"){
+        const website = "Youtube"
+        console.log(username + website);      
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+// ==================================================================================================
+// 
+function addOne(num){
+    return num + 1
+}
+// addOne(5)
+console.log(addOne(5));
+
+
+const addTwo = function(num){
+    return num + 2
+}
+// addTwo(5)
+console.log(addTwo(5));
