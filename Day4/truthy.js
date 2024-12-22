86f66842285b1c9417bcca3da8e699904e7f85f2
qiktech <prassaw@gmail.com>
2024-12-23 02:53:29 +0530

    Truthy improvement

--- Day4/truthy.js
+++ Day4/truthy.js
@@ -1,3 +1,17 @@
+// Step 81
+
+// The text has appeared again! This is because "false" is a string, which when evaluated to a boolean becomes true.
+// This means "false" is a truthy value.
+
+// A  truthy value is a value that is considered true when evaluated as a boolean. 
+// Most of the values you encounter in JavaScript will be truthy.
+
+// A falsy value is the opposite - a value considered false when evaluated as a boolean. 
+// JavaScript has a defined list of falsy values. Some of them include false, 0, "", null, undefined, and NaN.
+
+// Try changing your if condition to an empty string "", which is a falsy value.
+
+
 /* 
 TRUTHY and FALSY Values
 falsy values
---
+++ functionsExercises.js
@@ -0,0 +1,29 @@
+// const myNewArray = [100,200,400,600]
+
+// function myFunction() {
+//     return myNewArray[1]
+// }
+
+// console.log(myFunction());
+
+function reverseString(stringToReverse) {
+
+    let str = "PRASADD"
+    const reversed = str.split("").reverse().join("")
+    console.log(reversed);
+    // return reverseString
+
+
+    // let newString = ""
+    // for(let i=stringToReverse.length -1 ; i>=0; i--){
+    //     newString += stringToReverse[i]
+    // }
+    // return newString
+}
+
+// console.log(reverseString("priya"));
+reverseString()
+
+
+
+
